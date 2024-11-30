1c9247d593635b68854b4391f76130265447b288
Phong Truong <khamtruong1114@gmail.com>
2024-11-30 13:31:29 +0700

    changetoGray

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ function App() {
   const [disabled, setDisabled] = React.useState(false);
   const [buttonColor, setButtonColor] = useState("red");
   const nextColor = buttonColor === "red" ? "blue" : "red";
+  const className = disabled ? "gray" : buttonColor;
   // function handleClick() {
   //   setButtonColor((curColor) => (curColor === "red" ? "blue" : "red"));
   // }
   return (
     <div>
       <button
+        className={className}
         disabled={disabled}
-        className={buttonColor}
         onClick={() => setButtonColor(nextColor)}
       >
         Change to {nextColor}
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,7 +9,8 @@ test("button click flow", () => {
   const buttonElement = screen.getByRole("button", { name: /blue/i });
   //check initial color
   expect(buttonElement).toHaveClass("red");
-  //click the butotn
+  expect(checkboxElement);
+  //click the button
   fireEvent.click(buttonElement);
   //check button text
   expect(buttonElement).toHaveTextContent(/red/i);
@@ -24,13 +25,37 @@ test("checkbox flow", () => {
   const checkboxElement = screen.getByRole("checkbox", {
     name: /disable button/i,
   });
+
   //check initial conditions
   expect(buttonElement).toBeEnabled();
   expect(checkboxElement).not.toBeChecked();
+
   //click checkbox to disable button
   fireEvent.click(checkboxElement);
   expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
   //click checkbox again to re-enable button
   fireEvent.click(checkboxElement);
   expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("red");
+});
+test("checkbox flow after button clicked", () => {
+  render(<App />);
+  //find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  //click button to change to blue
+  fireEvent.click(buttonElement);
+  //click checkbox to disable button
+  expect(buttonElement).toBeDisabled();
+  expect(checkboxElement).toBeChecked();
+  expect(buttonElement).toHaveClass("gray");
+
+  //click checkbox to re-enbale button
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("blue");
 });
